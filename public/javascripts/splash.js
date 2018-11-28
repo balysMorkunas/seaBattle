72ee6467751d08783abc2d0a1f3a158c27bcb746
mateicristea88 <mateicristeaenache88@gmail.com>
2018-11-28 11:37:44 +0100

    copy paste works wonders

--- public/javascripts/splash.js
+++ public/javascripts/splash.js
@@ -34,9 +34,28 @@ document.getElementById("table1").onmouseover = function (event) {
     }
 }
 
-document.getElementById("table1").onmouseout = function (event) {
+//document.getElementById("table1").onmouseout = function (event) {
+//
+//    let target = event.target;
+//    makeWhite(target.id);
+//}
 
+document.getElementById("table1").onmouseout = function (event) {
     let target = event.target;
-    makeWhite(target.id);
+    if (target.className != "rowTop" && !target.className.includes("col0")) {
+
+        //this need to be remade, now it makes 3 cubes green, but they do not change back to white.
+        //Although, the slice function is good for this situation (aka changing the id number)
+        //I'm tired now, will approach this tommorrow.
+        let k = parseInt(target.id.charAt(1));
+        for(let i = parseInt(target.id.charAt(1));i<3 + k; i++){
+            let x = target.id;
+            x = x.slice(0, -1);
+
+            makeWhite(x+i);
+        }
+    }
 }
+
+
 
