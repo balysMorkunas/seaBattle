60772c25769758a5abe0260bac0b4708db00e880
balysMorkunas <balys.morkunas@gmail.com>
2018-12-16 19:03:51 +0100

    css

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var express = require("express");
 var http = require("http");
 var indexRouter = require("./routes/index");
-//var messages = require("./public/javascripts/messages");
+var messages = require("./public/javascripts/messages");
 var websocket = require("ws");
 var Game = require("./public/game");
 
@@ -35,10 +35,10 @@ var connectionID = 0; //each websocket recieves unique ID.
 //This function is used to check whether an accurate shot was made.
 function checkShot(fleet1, coordinate) {
 
-	fleet1 = JSON.stringify(fleet1);
+  fleet1 = JSON.stringify(fleet1);
 
-	if (fleet1.includes(coordinate)) return true;
-	else return false;
+  if (fleet1.includes(coordinate)) return true;
+  else return false;
 }
 
 
@@ -46,112 +46,109 @@ function checkShot(fleet1, coordinate) {
 
 wss.on("connection", function connection(ws) {
 
-	// setInterval(function () {
-	//   console.log("Connection state: " + ws.readyState);
-	//   ws.send("Thanks for the message. -- Server.");
-	//   ws.close();
-	//   console.log("Connection state: " + ws.readyState);
-	// }, 2000);
-	// ws.on("message", function incoming(message) {
-	//   console.log("[LOG]" + message);
+  // setInterval(function () {
+  //   console.log("Connection state: " + ws.readyState);
+  //   ws.send("Thanks for the message. -- Server.");
+  //   ws.close();
+  //   console.log("Connection state: " + ws.readyState);
+  // }, 2000);
+  // ws.on("message", function incoming(message) {
+  //   console.log("[LOG]" + message);
 
-	// });
+  // });
 
-	let con = ws;
-	con.id = connectionID++;
+  let con = ws;
+  con.id = connectionID++;
 
-	ws.onmessage = function (event) {
+  ws.onmessage = function (event) {
 
-		//console.log(event.data);
-		if (event.data === "READY") {
-			let playerType = currentGame.addPlayer(con);
+    var flag = 0;
+    //console.log(event.data);
+    if (event.data === "READY") {
+      let playerType = currentGame.addPlayer(con);
 
-			if (playerType === "A") {
+      if (playerType === "A") {
 
-				ws.send("A_GAME");
+        ws.send("A_GAME");
 
-			}
-			if (playerType === "B") {
-				ws.send("B_GAME_START");
-			}
-			if (playerType === "N") {
+      }
+      if (playerType === "B") {
+        ws.send("B_GAME_START");
+      }
+      if (playerType === "N") {
 
-				currentGame = new Game(id++);
-				playerType = currentGame.addPlayer(con);
-				ws.send("A_GAME");
-			}
-			websockets[con.id] = currentGame;
-			//console.log(playerType);
-			//console.log(websockets[con.id]);
-			//console.log(con.id);
+        currentGame = new Game(id++);
+        playerType = currentGame.addPlayer(con);
+        ws.send("A_GAME");
+      }
+      websockets[con.id] = currentGame;
+      //console.log(playerType);
+      //console.log(websockets[con.id]);
+      //console.log(con.id);
+      console.log("Player %s placed in game %s as %s", con.id, currentGame.id, playerType);
+    }
 
-			console.log("Player %s placed in game %s as %s", con.id, currentGame.id, playerType);
-		}
+    // This waits for the fleet objects and converts them to Fleet objects.
+    if (event.data.includes("**A**")) {
+      // var fleetA = new Fleet(con.id);
+      fleetA = event.data;
+      fleetA = fleetA.slice(5);
+      fleetA = JSON.parse(fleetA);
 
+      //console.log(fleetA);
 
-		// This waits for the fleet objects and converts them to Fleet objects.
-		if (event.data.includes("**A**")) {
-			// var fleetA = new Fleet(con.id);
-			fleetA = event.data;
-			fleetA = fleetA.slice(5);
-			fleetA = JSON.parse(fleetA);
+    }
+    if (event.data.includes("**B**")) {
+      // var fleetB = new Fleet(con.id);
+      fleetB = event.data;
+      fleetB = fleetB.slice(5);
+      fleetB = JSON.parse(fleetB);
 
-			//console.log(fleetA);
+      //console.log(fleetB);
 
-		}
-		if (event.data.includes("**B**")) {
-			// var fleetB = new Fleet(con.id);
-			fleetB = event.data;
-			fleetB = fleetB.slice(5);
-			fleetB = JSON.parse(fleetB);
+    }
 
-			//console.log(fleetB);
+    //This waits for a coordinate, checks if its hit or miss and responds back to the shooter 
+    if (event.data.includes("A_TARGET")) {
 
-		}
+      let gameObj = websockets[con.id];
 
-		//This waits for a coordinate, checks if its hit or miss and responds back to the shooter 
-		if (event.data.includes("A_TARGET")) {
+      //console.log(gameObj);
+      var coordinate = event.data.slice(9);
+      if (checkShot(fleetB, coordinate)) {
+        gameObj.playerA.send("A_HIT_" + coordinate);
+        gameObj.playerB.send("A_HIT_" + coordinate);
 
-			let gameObj = websockets[con.id];
+        gameObj.countA++;
+        if(gameObj.countA == 17) gameObj.playerA.send("A_WIN");
+        console.log("A HIT");
+      }
+      else {
+        gameObj.playerA.send("A_MISS_" + coordinate);
+        gameObj.playerB.send("A_MISS_" + coordinate);
+        console.log("A miss");
+      }
 
-			//console.log(gameObj);
-			var coordinate = event.data.slice(9);
-			if (checkShot(fleetB, coordinate)) {
-				gameObj.playerA.send("A_HIT_" + coordinate);
-				gameObj.playerB.send("A_HIT_" + coordinate);
+    }
+    if (event.data.includes("B_TARGET")) {
+      let gameObj = websockets[con.id];
+      var coordinate = event.data.slice(9);
+      if (checkShot(fleetA, coordinate)) {
+        gameObj.playerA.send("B_HIT_" + coordinate);
+        gameObj.playerB.send("B_HIT_" + coordinate);
 
-				gameObj.countA++;
-				if (gameObj.countA == 17) {
-					gameObj.playerA.send("A_WIN");
-					gameObj.playerB.send("A_WIN");
-				}
-			}
-			else {
-				gameObj.playerA.send("A_MISS_" + coordinate);
-				gameObj.playerB.send("A_MISS_" + coordinate);
-			}
+        gameObj.countB++;
+        if(gameObj.countB == 17) gameObj.playerB.send("B_WIN");
+        console.log("B HIT");
 
-		}
-		if (event.data.includes("B_TARGET")) {
-			let gameObj = websockets[con.id];
-			let coordinate = event.data.slice(9);
-			if (checkShot(fleetA, coordinate)) {
-				gameObj.playerA.send("B_HIT_" + coordinate);
-				gameObj.playerB.send("B_HIT_" + coordinate);
-
-				gameObj.countB++;
-				if (gameObj.countB == 17) {
-					gameObj.playerB.send("B_WIN");
-					gameObj.playerA.send("B_WIN");
-				}
-
-			}
-			else {
-				gameObj.playerA.send("B_MISS_" + coordinate);
-				gameObj.playerB.send("B_MISS_" + coordinate);
-			}
-		}
-	};
+      }
+      else {
+        gameObj.playerA.send("B_MISS_" + coordinate);
+        gameObj.playerB.send("B_MISS_" + coordinate);
+        console.log("B miss");
+      }
+    }
+  }
 
 });
 
